2584773b0914987ceb0d88a6a1f2a062df8c112e
Mohammad Hashem Nowruzi <PersonalHashem@gmail.com>
2021-09-12 20:08:03 +0430

    Read command line arguments with 'PKG' class

--- pkg/__main__.py
+++ pkg/__main__.py
@@ -1,5 +1,19 @@
+import sys
+
+
+class PKG:
+    def __init__(self, argv):
+        self.argv = argv if argv else None
+
+    def execute(self) -> None:
+        if self.argv is None:
+            return
+        pass
+
+
 def main():
-    pass
+    pkg = PKG(sys.argv[1:])
+    pkg.execute()
 
 
 if __name__ == '__main__':
