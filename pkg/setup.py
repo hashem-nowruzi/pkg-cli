e674ee933f7c34b06665c929faa91b204fc98414
Mohammad Hashem Nowruzi <PersonalHashem@gmail.com>
2021-09-13 20:37:01 +0430

    refa: Put two commands inside the if __name__ == '__main__' block

--- pkg/setup.py
+++ pkg/setup.py
@@ -1,6 +1,7 @@
 from .commands import BaseCommand
 from setuptools import setup
 
-config = BaseCommand.get_user_config()
 
-setup(**config)
+if __name__ == '__main__':
+    config = BaseCommand.get_user_config()
+    setup(**config)
