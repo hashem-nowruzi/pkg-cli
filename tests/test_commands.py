a8a342595280e3bc499fa995a9450332374446b4
Mohammad Hashem Nowruzi <PersonalHashem@gmail.com>
2021-09-21 11:39:06 +0430

    test(init): Test whether the 'package.json' file is created or not?

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,10 +1,24 @@
 import unittest
 from pkg.__main__ import PKG
+from pathlib import Path
+
+HERE = Path(__file__).parent
 
 
 class BaseCommandTest(unittest.TestCase):
     def setUp(self) -> None:
         self.parser = PKG().parser
+
+    def tearDown(self) -> None:
+        (HERE / 'package.json').unlink(missing_ok=True)
+
+
+class InitCommandTest(BaseCommandTest):
+    def test_package_json(self):
+        args = self.parser.parse_args(['init'])
+        args.func(args)
+
+        self.assertTrue((HERE / 'package.json').is_file())
 
 
 if __name__ == '__main__':
