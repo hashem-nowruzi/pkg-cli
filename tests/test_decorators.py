d1abb053b9d776a76a766da5e860e38789352ea6
Mohammad Hashem Nowruzi <PersonalHashem@gmail.com>
2021-09-21 18:26:35 +0430

    test: Add a test for 'package_json_required' function

---
+++ tests/test_decorators.py
@@ -0,0 +1,22 @@
+from pkg.decorators import package_json_required
+from utils import CapturingOutput
+from test_commands import HERE
+import unittest
+
+
+class DecoratorTest(unittest.TestCase):
+    def test_package_json_required(self):
+        func = package_json_required(lambda: print('Function Done!'))
+
+        with CapturingOutput() as output:
+            (HERE / 'package.json').touch(exist_ok=True)
+            func()
+            (HERE / 'package.json').unlink(missing_ok=True)
+            func()
+
+        self.assertEqual(output[0], 'Function Done!')
+        self.assertEqual(output[1], 'Error: The "package.json" file is required!!')
+
+
+if __name__ == '__main__':
+    unittest.main()
