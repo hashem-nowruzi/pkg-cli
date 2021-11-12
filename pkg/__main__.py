da0ee59e8d0e1757edb4d56b5b32ada2960b28bf
githashem <PersonalHashem@gmail.com>
2021-11-12 19:37:25 +0330

    feat: add 'freeze' command

--- pkg/__main__.py
+++ pkg/__main__.py
@@ -1,4 +1,4 @@
-from .commands import init, publish
+from .commands import init, publish, freeze
 from argparse import ArgumentParser
 from . import __version__
 
@@ -12,6 +12,8 @@ def get_parser():
     parser_init.set_defaults(func=init)
     parser_publish = subparsers.add_parser('publish', help='Publish package to PyPi')
     parser_publish.set_defaults(func=publish)
+    parser_freeze = subparsers.add_parser('freeze', help='Output only top-level packages in requirements format')
+    parser_freeze.set_defaults(func=freeze)
 
     return parser
 
